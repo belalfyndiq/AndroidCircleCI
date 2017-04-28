3489b08b92d56c287d5a11475d322ac5d8a84a03
Belal Mohamed <belal.mohamed@fyndiq.com>
2017-04-28 16:05:59 +0200

    Dummy commit to add123

--- app/src/main/java/com/example/belalmohamed/androidcircleci/MainActivity.java
+++ app/src/main/java/com/example/belalmohamed/androidcircleci/MainActivity.java
@@ -13,6 +13,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        
     }
 
 
