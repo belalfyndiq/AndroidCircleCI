f06789706838b9e3c86adcc19602756c51fef04f
Belal Mohamed <belal.mohamed@fyndiq.com>
2017-04-28 12:02:52 +0200

    Add main activity

---
+++ app/src/main/java/com/example/belalmohamed/androidcircleci/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.belalmohamed.androidcircleci;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by belal.mohamed on 2017-04-28.
+ * www.fyndiq.com
+ */
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
