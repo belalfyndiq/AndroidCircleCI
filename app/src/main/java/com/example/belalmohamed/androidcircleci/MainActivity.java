b630a38bea012ed2743c24be404fba23c89ca4ad
Belal Mohamed <belal.mohamed@fyndiq.com>
2017-05-01 13:34:19 +0200

    Remove assembleDebug from Circle.yml and add Fabric crashlytics

--- app/src/main/java/com/example/belalmohamed/androidcircleci/MainActivity.java
+++ app/src/main/java/com/example/belalmohamed/androidcircleci/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.belalmohamed.androidcircleci;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by belal.mohamed on 2017-04-28.
@@ -13,5 +15,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
     }
 }
